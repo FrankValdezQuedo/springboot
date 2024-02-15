91809dd8a03d91714a1bc9a15d7a5d7f515fe698
Frank <frank24111999@gmail.com>
2024-02-15 12:52:26 -0500

    se implemento el login con la base de datos

--- src/main/java/cursoJava/curso/dao/UsuarioDaoImpl.java
+++ src/main/java/cursoJava/curso/dao/UsuarioDaoImpl.java
@@ -34,4 +34,19 @@ public class UsuarioDaoImpl implements UsuarioDao{
     entityManager.merge(usuario);
     }
 
+    @Override
+    public boolean verificarCredenciales(Usuario usuario) {
+        String query = "From Usuario WHERE email = :email AND password = :password";
+        List<Usuario> lista =  entityManager.createQuery(query)
+                .setParameter("email",usuario.getEmail())
+                .setParameter("password",usuario.getPassword())
+                .getResultList();
+        if(lista.isEmpty()){
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+
 }
